f2fca2b676276e3f88aac24226db8b0084ae1bb4
TannerHelms <tannerhelms01@gmail.com>
2024-03-16 20:14:37 -0600

    added ids to new chats

--- src/api/createChat.js
+++ src/api/createChat.js
@@ -13,11 +13,14 @@ const CreateChat = async (usera, userb) => {
         await setDoc(doc(db, "chats", newUuid), {
             chat_group_id: newUuid,
             user_a: doc(db, "users", usera),
+            user_a_id: usera,
+            user_b_id: userb,
             user_b: doc(db, "users", userb),
             users: [doc(db, "users", usera), doc(db, "users", userb)],
             last_message: "Chat Created",
             last_message_time: timestamp,
             timestamp,
+
         });
 
         return { success: true }
